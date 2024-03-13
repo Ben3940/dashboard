ef9e4cd7671d78ba29722f2a24deaeccada56c3c
Ben3940 <bty2015@comcast.net>
2024-03-13 17:41:31 -0400

    Bar_Chart components fetches and loads data into bar chart using Charts.js to display on dashboard

--- client/src/components/Bar_Chart.tsx
+++ client/src/components/Bar_Chart.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Chart from 'chart.js/auto';
 import { ChartItem } from 'chart.js/auto';
+import { generic_fetch } from '../utils/Generic_Fetch';
 
 export function BarChart() {
-  const labels = ['a', 'b', 'c', 'd'];
-  const values = [2, 4, 1, 6];
+  const [labels, setLabels] = useState([]);
+  const [values, setValues] = useState([]);
+
+  const load_data = async () => {
+    const data = await generic_fetch(
+      `query Data {
+        category_quantities {
+          Category,
+          Quantity
+        }
+      }
+      `,
+      'category_quantities'
+    );
+    let label_arr: String[] = [];
+    let value_arr: String[] = [];
+    console.log(data);
+    data.forEach((item) => {
+      label_arr.push(item.Category);
+      value_arr.push(item.Quantity);
+    });
+    setLabels(label_arr);
+    setValues(value_arr);
+  };
+  useEffect(() => {
+    load_data();
+  }, []);
   (async function () {
     new Chart(document.querySelector('#categories-quantities') as ChartItem, {
       type: 'bar',
