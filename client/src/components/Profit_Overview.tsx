c0f7b89582f82458c7c64245628ff81caa8efa55
Ben3940 <bty2015@comcast.net>
2024-03-16 19:46:22 -0400

    Added Profit_Overview component to display total profits and quantity of items sold

---
+++ client/src/components/Profit_Overview.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import { generic_fetch } from '../utils/Generic_Fetch';
+
+interface Profit_Quantity {
+  Profit: number;
+  Quantity: number;
+}
+
+export function Profit_Overview() {
+  const [profitQuantity, setProfitQuantity] = useState<Profit_Quantity>({
+    Profit: 0,
+    Quantity: 0,
+  });
+
+  const load_data = async () => {
+    const data = await generic_fetch(
+      `query Total_Profits_Quantities {
+        get_total_profits_quantities {
+                Profit
+                Quantity
+            }
+        }
+        `,
+      'get_total_profits_quantities'
+    );
+    setProfitQuantity(data[0]);
+  };
+
+  useEffect(() => {
+    load_data();
+  }, []);
+  return (
+    <section className='card'>
+      <h1>Profit: {profitQuantity.Profit}</h1>
+      <h1>Quantity: {profitQuantity.Quantity}</h1>
+    </section>
+  );
+}
